a6e3ec4cabf250bec77f9dabd4728ee284312961
Allwin112 <Allwin112@users.noreply.github.com>
2017-04-03 01:37:06 +0530

    Update Day_12_Inheritance.cpp

--- Day_12_Inheritance.cpp
+++ Day_12_Inheritance.cpp
@@ -25,26 +25,11 @@ class Student :  public Person{
 	private:
 		vector<int> testScores;
 	public:
-        /*
-        *   Class Constructor
-        *
-        *   Parameters:
-        *   firstName - A string denoting the Person's first name.
-        *   lastName - A string denoting the Person's last name.
-        *   id - An integer denoting the Person's ID number.
-        *   scores - An array of integers denoting the Person's test scores.
-        */
-        // Write your constructor here
         Student(string firstName, string lastName, int id, vector<int> scores)
         :Person(firstName, lastName, id)
             {
             testScores = scores;
 		}
-        /*
-        *   Function Name: calculate
-        *   Return: A character denoting the grade.
-        */
-        // Write your function here
         char calculate()
             {
             int sum = 0, avg = 0, i;
